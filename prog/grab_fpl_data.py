bfe503851ffd195b93c592fa1720d5e8932d6670
Gabriel <gbarajas@alumni.stanford.edu>
2026-01-11 14:17:31 -0800

    Initial check-in.

---
+++ prog/grab_fpl_data.py
@@ -0,0 +1,14 @@
+import sys
+import requests
+import json
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1 and sys.argv[1] == '-h':
+		print('Function: Grabs all data from the FPL website and dumps it to a JSON file.')
+		sys.exit()
+
+	fplUrl = 'https://fantasy.premierleague.com/api/'
+	data = requests.get(fplUrl+'bootstrap-static/').json()
+	outFn = 'fpl_data.json'
+	with open(outFn, 'w') as fOut:
+		json.dump(outFn, fOut)
